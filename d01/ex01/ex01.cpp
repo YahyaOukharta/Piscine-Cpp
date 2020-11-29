25faa4fc8e71c05d2eb6a4f53d230fa294984430
Yahya Oukharta <youkhart@e1r9p12.1337.ma>
2020-11-29 03:46:33 +0100

    d01/ex01

---
+++ d01/ex01/ex01.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+
+void memoryLeak()
+{
+    std::string* panthere = new std::string("String panthere");
+    std::cout << *panthere << std::endl;
+    delete panthere;
+}
