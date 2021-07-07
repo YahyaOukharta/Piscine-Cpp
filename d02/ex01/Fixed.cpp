5bbaad724af67a27662064814587880c73188a4b
Yahya Oukharta <youkhart@e0r5p7.1337.ma>
2021-07-07 20:09:10 +0100

    update

--- d02/ex01/Fixed.cpp
+++ d02/ex01/Fixed.cpp
@@ -1,12 +1,5 @@
 #include "Fixed.hpp"
 
-int power (int n, int to)
-{
-    if (to == 0)
-        return (1);
-    return (n * pow(n, to - 1));
-}
-
 Fixed::Fixed(void) : val(0)
 {
     std::cout << "Default constructor called" << std::endl;
@@ -21,23 +14,16 @@ Fixed::Fixed(const Fixed &n)
 Fixed::Fixed(const int n)
 {
     std::cout << "Int constructor called" << std::endl;
-    val = n;
-    val <<= n_fractional_bits;
+    val = n << n_fractional_bits;
 }
 
 Fixed::Fixed(const float n)
 {
     std::cout << "Float constructor called" << std::endl;
-    int real = (int)std::roundf(n);
-    int floating = int(n * 1000) % 1000;
-    if (floating >= power(2,this->n_fractional_bits))
-        floating /= 10;
-    std::cout << floating << std::endl;
-    this->setRawBits(real);
-    this->val <<= this->n_fractional_bits;
-    this->val += floating;
-    // val <<= n_fractional_bits;
-    // val += (n * 100) % 256;
+
+    float tmp = n * (1 << this->n_fractional_bits);
+
+    this->setRawBits((int)std::roundf(tmp));
 }
 
 Fixed::~Fixed(void)
@@ -65,25 +51,18 @@ int Fixed::toInt(void) const
 
 float Fixed::toFloat(void) const
 {
-    float res = this->getRawBits() >> this->n_fractional_bits;
-    int fract = this->getRawBits() % power(2, this->n_fractional_bits);
-
-    float tmp = (float)fract;  // 0.423  0.42
-
-    tmp /= 100;
-
-    return (res + tmp);
+    return ((float)this->getRawBits() / (float)(1 << this->n_fractional_bits));
 }
 
 void Fixed::operator = (const Fixed &n)
 {
     std::cout << "Assignation operator called" << std::endl;
+
     this->setRawBits(n.getRawBits());
 }
 
 std::ostream &operator<<(std::ostream& os, const Fixed &n)
 {
-    //os << (val >> n_fractional_bits) << "." << (val )
     os << n.toFloat();
     return (os);
 }
--- d02/ex01/main.cpp
+++ d02/ex01/main.cpp
@@ -1,11 +1,12 @@
 #include "Fixed.hpp"
 
-int main( void ) {
+int main(void)
+{
     Fixed a;
     Fixed const b( 10 );
     Fixed const c( 42.42f );
     Fixed const d( b );
-    a = Fixed( 1234.1f );
+    a = Fixed( 1234.4321f );
 
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
