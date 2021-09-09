a438e0e299555b69891bd46159e34d3d0cacec2f
Yahya Oukharta <youkhart@e2r6p12.1337.ma>
2021-09-09 16:50:45 +0100

    update

--- d08/ex01/Span.cpp
+++ d08/ex01/Span.cpp
@@ -1,29 +1,88 @@
 #include "Span.hpp"
 
 
-Span::Span()
+Span::Span(unsigned int N): n(N)
 {
 }
 
-Span::Span( const Span & src )
+Span::Span( const Span & src ): n(src.n)
 {
+	for(size_t i = 0; i < src.size(); i++)
+		vec.push_back(src.getElement(i));
 }
 
 Span::~Span()
 {
 }
-
-Span &				Span::operator=( Span const & rhs )
+int Span::getElement(unsigned int idx) const
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	if (size() >= n)
+		throw OutOfBoundsException;
+	return(vec[idx]);
+}
+
+size_t min(size_t a, unsigned int b)
+{
+	if ( a < b)
+		return (a);
+	else 
+		return (b);
+}
+
+size_t Span::size() const
+{
+	return (min(vec.size(), n));
+}
+
+void Span::addNumber(int a)
+{
+	if (size() < n)
+		vec.push_back(a);
+	else
+		throw OutOfBoundsException;
+}
+
+void Span::setN(int _n)
+{
+	n = _n;
+}
+
+int Span::getN(void) const
+{
+	return n;
+}
+int Span::shortestSpan(void) 
+{
+	tmp.clear();
+	std::copy(vec.begin(), vec.end(), tmp.begin());
+	std::sort(tmp.begin(), tmp.end());
+
+	return n;
+}
+int Span::longestSpan(void) 
+{
+	tmp.clear();
+	for(size_t i = 0; i < size(); i++)
+	{	
+		std::cout << i << std::endl;
+		tmp.push_back(vec[i]);
+	}
+	std::sort(tmp.begin(), tmp.end());
+
+	return (tmp[tmp.size() - 1] - tmp[0]);
+}
+
+Span &				Span::operator=( Span const & src )
+{
+	vec.clear();
+	this->setN(src.getN()); 
+	for(size_t i = 0; i < src.size(); i++)
+		vec.push_back(src.getElement(i));
 	return *this;
 }
 
-std::ostream &			operator<<( std::ostream & o, Span const & i )
-{
-	//o << "Value = " << i.getValue();
-	return o;
-}
+// std::ostream &			operator<<( std::ostream & o, Span const & i )
+// {
+// 	//o << "Value = " << i.getValue();
+// 	return o;
+// }
--- d08/ex01/Span.hpp
+++ d08/ex01/Span.hpp
@@ -3,19 +3,43 @@
 
 # include <iostream>
 # include <string>
+# include <vector>
+#include <exception>
+#include <algorithm>
+
+class OutOfBoundsExcept: public std::exception
+{
+	const char* what() const throw()
+	{
+		return "Index Out of Bounds";
+	};
+};
 
 class Span
 {
+	private:
+		unsigned int n;
+		std::vector<int> vec;
+		std::vector<int> tmp;
 
+ 		OutOfBoundsExcept OutOfBoundsException;
 	public:
 
-		Span();
+		Span(unsigned int N);
 		Span( Span const & src );
 		~Span();
 
 		Span &		operator=( Span const & rhs );
 
-	private:
+		int getElement(unsigned int idx) const ;
+		size_t size() const ;
+		void addNumber(int a);
+
+		void setN(int n);
+		int	 getN()const ;
+		
+		int shortestSpan(void);
+		int longestSpan(void);
 
 };
 
